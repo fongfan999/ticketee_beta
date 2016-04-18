7b50a5f99f12272d8c71bebfc5957f8d4fc2ecab
Phong Phan Thai <fongfan999@gmail.com>
2016-04-18 14:14:52 +0700

    Implement creating tickets for a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,6 +46,7 @@ class ProjectsController < ApplicationController
 	end
 
 	private
+	
 	def project_params
 		params.require(:project).permit(:name, :description)
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412101329) do
+ActiveRecord::Schema.define(version: 20160418033453) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20160412101329) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
 
 end
