c7d91bd45b05b81bbae2b89b681b867ae8b235b9
Phong Phan Thai <fongfan999@gmail.com>
2016-05-01 10:47:16 +0700

    Display a comment's state transition

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,14 @@
 module TicketsHelper
+	def state_transition_for(comment)
+		if comment.previous_state != comment.state
+			content_tag(:p) do
+				value = "<strong><i class='fa fa-gears'> state changed</i></strong>"
+				if comment.previous_state.present?
+					value += " from #{render comment.previous_state}"
+				end
+				value += " to #{render comment.state}"
+				value.html_safe
+			end
+		end
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,15 +2,21 @@ class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: "User"
   belongs_to :state
+  belongs_to :previous_state, class_name: "State"
 
   validates :text, presence: true
   delegate :project, to: :ticket
 
   scope :persisted, lambda { where.not(id: nil) }
 
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   private
+  
+  def set_previous_state
+    self.previous_state = ticket.state
+  end
   
   def set_ticket_state
   	ticket.state = state
