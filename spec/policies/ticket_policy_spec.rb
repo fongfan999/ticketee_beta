8a8f29b6f0fb715d45349bfdf50f681fa2dbc09b
Phong Phan Thai <fongfan999@gmail.com>
2016-05-02 10:53:45 +0700

    Only admins and managers can change states of a ticket

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :create }
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
+      it { should_not permit_action :change_state }
     end
 
     context "for viewers of the project" do
@@ -29,6 +30,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :create }
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
+      it { should_not permit_action :change_state }
     end
 
     context "for editors of the project" do
@@ -38,6 +40,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :create }
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
+      it { should_not permit_action :change_state }
 
       context "when the editor created the ticket" do
         before { ticket.author = user }
@@ -53,6 +56,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :create }
       it { should permit_action :update }
       it { should permit_action :destroy }
+      it { should permit_action :change_state }
     end
 
     context "for managers of other projects" do
@@ -62,6 +66,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :create }
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
+      it { should_not permit_action :change_state }
     end
 
     context "for administrators" do
@@ -71,6 +76,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :create }
       it { should permit_action :update }
       it { should permit_action :destroy }
+      it { should permit_action :change_state }
     end
   end
 end
